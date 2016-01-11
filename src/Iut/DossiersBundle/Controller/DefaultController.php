1ba38a7994493a6bb196dc4579a12e256e7ff7ff
TZK- <thib-17@orange.fr>
2016-01-11 15:33:34 +0100

    Adding Vacataire entity

--- src/Iut/DossiersBundle/Controller/DefaultController.php
+++ src/Iut/DossiersBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace Iut\DossiersBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Iut\DossiersBundle\Entity\Vacataire;
+
 
 /**
  * @Route("/dossiers")
@@ -14,7 +16,11 @@ class DefaultController extends Controller {
      * @Route("/", name="homepage")
      */
     public function indexAction() {
-        return $this->render('IutDossiersBundle:Default:index.html.twig');
+        $vacataires = $this->getDoctrine()->getManager()->getRepository('IutDossiersBundle:Vacataire');
+
+        return $this->render('IutDossiersBundle:Default:index.html.twig',
+                ['vacataires' => $vacataires->findAll()]
+        );
     }
 
 }
---
+++ src/Iut/DossiersBundle/Entity/Vacataire.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Iut\DossiersBundle\Entity;
+
+/**
+ * Vacataire
+ */
+class Vacataire
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nom;
+
+    /**
+     * @var string
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     */
+    private $mail;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Vacataire
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Vacataire
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Vacataire
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+}
+
