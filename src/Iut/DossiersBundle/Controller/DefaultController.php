6cd373d12daf114381963994fc271b234b563086
klimczam <mathis.klimczak@iut-valence.fr>
2016-01-12 16:03:43 +0100

    Adding html page ajouterVacataire + routing (2)

--- src/Iut/DossiersBundle/Controller/DefaultController.php
+++ src/Iut/DossiersBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace Iut\DossiersBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Iut\DossiersBundle\Entity\Vacataire;
+use Iut\DossiersBundle\Form\VacataireType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller {
 
@@ -13,10 +16,25 @@ class DefaultController extends Controller {
         );
     }
 
-    public function ajouterVacataireAction() {
-        
+    public function ajouterVacataireAction(Request $request) {
 
-        return $this->render('IutDossiersBundle:Default:ajouterVacataire.html.twig');
+        $vacataire = new Vacataire();
+
+        $form = $this->createForm(VacataireType::class, $vacataire);
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            $entytymanager = $this->getDoctrine()->getManagerForClass(Vacataire::class);
+            $entytymanager->persist($vacataire);
+            $entytymanager->flush();
+            $this->addFlash('success', 'Le vacataire a bien été ajouté');
+            return $this->redirectToRoute('homepage');
+                    
+        }
+          
+        return $this->render('IutDossiersBundle:Default:ajouterVacataire.html.twig', [
+                    'title' => "Ajouter un vacataire",
+                    'form' => $form->createView()
+        ]);
     }
 
 }
--- src/Iut/DossiersBundle/Form/VacataireType.php
+++ src/Iut/DossiersBundle/Form/VacataireType.php
@@ -5,29 +5,31 @@ namespace Iut\DossiersBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use \Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class VacataireType extends AbstractType
-{
+class VacataireType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('mail')
-        ;
+                ->add('nom')
+                ->add('prenom')
+                ->add('mail')
+                ->add('submit', SubmitType::class, [
+                    'attr' => ['class' => "btn btn-primary"]
+                ]);
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Iut\DossiersBundle\Entity\Vacataire'
         ));
     }
+
 }
