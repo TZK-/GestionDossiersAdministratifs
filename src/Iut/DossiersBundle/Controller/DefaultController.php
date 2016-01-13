a3ea05b7ed4d568cfad73992cdb7038280275930
TZK <thib-17@orange.fr>
2016-01-13 13:38:27 +0100

    Adding title to the index page

--- src/Iut/DossiersBundle/Controller/DefaultController.php
+++ src/Iut/DossiersBundle/Controller/DefaultController.php
@@ -12,8 +12,10 @@ class DefaultController extends Controller {
     public function indexAction() {
         $vacataires = $this->getDoctrine()->getManager()->getRepository('IutDossiersBundle:Vacataire');
 
-        return $this->render('IutDossiersBundle:Default:index.html.twig', ['vacataires' => $vacataires->findAll()]
-        );
+        return $this->render('IutDossiersBundle:Default:index.html.twig', [
+                    'title' => "Accueil",
+                    'vacataires' => $vacataires->findAll()
+        ]);
     }
 
     public function ajouterVacataireAction(Request $request, $id) {
@@ -21,16 +23,15 @@ class DefaultController extends Controller {
         $vacataire = new Vacataire();
 
         $form = $this->createForm(VacataireType::class, $vacataire);
-        if($request->isMethod('POST')){
+        if ($request->isMethod('POST')) {
             $form->handleRequest($request);
             $entytymanager = $this->getDoctrine()->getManagerForClass(Vacataire::class);
             $entytymanager->persist($vacataire);
             $entytymanager->flush();
             $this->addFlash('success', 'Le vacataire a bien été ajouté');
             return $this->redirectToRoute('homepage');
-                    
         }
-          
+
         return $this->render('IutDossiersBundle:Default:ajouterVacataire.html.twig', [
                     'title' => "Ajouter un vacataire",
                     'form' => $form->createView()
