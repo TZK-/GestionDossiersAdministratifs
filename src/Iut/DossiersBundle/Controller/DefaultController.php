c0350f88282f23fa8a412fd6ac182230b0393853
Théo <theo.zurcher@P=iut-valence.fr>
2016-01-13 18:50:30 +0100

    Creating the form for modeleMail and the page consulterModeleMail.html.twig Updating routing.yml and DefaultController.php

--- src/Iut/DossiersBundle/Controller/DefaultController.php
+++ src/Iut/DossiersBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace Iut\DossiersBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Iut\DossiersBundle\Entity\Vacataire;
 use Iut\DossiersBundle\Form\VacataireType;
+use Iut\DossiersBundle\Entity\ModeleMail;
+use Iut\DossiersBundle\Form\ModeleMailType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller {
@@ -35,6 +37,36 @@ class DefaultController extends Controller {
                     'title' => "Ajouter un vacataire",
                     'form' => $form->createView()
         ]);
+    }
+
+    public function ajouterModeleMailAction(Request $request) {
+
+        $mail = new ModeleMail();
+
+        $form = $this->createForm(ModeleMailType::class, $mail);
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            $entytymanager = $this->getDoctrine()->getManagerForClass(ModeleMail::class);
+            $entytymanager->persist($mail);
+            $entytymanager->flush();
+            $this->addFlash('success', 'Le modèle de mail a bien été ajouté');
+            return $this->redirectToRoute('homepage');
+
+        }
+
+        return $this->render('IutDossiersBundle:Default:ajouterModeleMail.html.twig', [
+                    'title' => "Ajouter un modèle de mail",
+                    'form' => $form->createView()
+        ]);
+    }
+    
+    public function consulterModeleMailAction() {
+        $mail = $this->getDoctrine()->getManager()->getRepository('IutDossiersBundle:ModeleMail');
+
+        return $this->render('IutDossiersBundle:Default:consulterModeleMail.html.twig', [
+            'title' => "Ajouter un modèle de mail",
+            'modeleMail' => $mail->findAll()]
+        );
     }
 
 }
