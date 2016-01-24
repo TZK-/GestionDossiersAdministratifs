0c1f05121740b51e74b24aee376dd8f4e3117e04
Mathie <Mathie@mbp-de-mathie>
2016-01-24 17:41:32 +0100

    ajout de code de test : rempli les tables dossier, vacataire, formation + affiche informations sur la page d'accueil lien : GestionDossiersAdministratifs/web/app_dev.php/dossiers/vacataire/ajoutertest

--- src/Iut/DossiersBundle/Controller/VacataireController.php
+++ src/Iut/DossiersBundle/Controller/VacataireController.php
@@ -7,6 +7,7 @@ use Iut\DossiersBundle\Entity\Vacataire;
 
 use Iut\DossiersBundle\Entity\Formation;
 use Iut\DossiersBundle\Entity\Etat;
+use Iut\DossiersBundle\Entity\Dossier;
 use Iut\DossiersBundle\Form\VacataireType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,31 +18,64 @@ class VacataireController extends Controller {
         return $this->render("IutDossiersBundle:Vacataire:listeVacataires.html.twig", [
             'vacataires' => $entityManager->getRepository(Vacataire::class)->findAll(),
             'formations' => $entityManager->getRepository(Formation::class)->findAll(),
-            'etats' => $entityManager->getRepository(Etat::class)->findAll()
+            'etats' => $entityManager->getRepository(Etat::class)->findAll(),
+            'dossiers' => $entityManager->getRepository(Dossier::class)->findAll()
         ]);
     }
 
     /* TESTS */
 
-    private function addVacTest(){
-        $vacataire = new Vacataire();
-        $vacataire->setNom("Albert");
-        $vacataire->setPrenom("Jean");
-        $vacataire->setMail("jean.albert@gmail.com");
+    public function addVacTestAction() {
+        $vacataire1 = new Vacataire();
+        $vacataire1->setNom("Albert");
+        $vacataire1->setPrenom("Jean");
+        $vacataire1->setMail("jean.albert@gmail.com");
+        
+        $vacataire2 = new Vacataire();
+        $vacataire2->setNom("Hugue");
+        $vacataire2->setPrenom("Edward");
+        $vacataire2->setMail("edward.hugue@hotmail.fr");
 
-        $formations = new \Iut\DossiersBundle\Entity\Formation;
-        $formations->setLibelle("Informatique");
+        $formations1 = new Formation;
+        $formations1->setLibelle("Informatique");
+        $vacataire1->addFormation($formations1);
 
-        $vacataire->addFormation($formations);
+        $formations2 = new Formation;
+        $formations2->setLibelle("TC");
+        $vacataire2->addFormation($formations2);
 
-        $formations = new \Iut\DossiersBundle\Entity\Formation;
-        $formations->setLibelle("GEA");
+        $formations = new Formation;
+        $formations->setLibelle("GEA");  
+        $vacataire2->addFormation($formations);
+        $vacataire1->addFormation($formations);
 
-        $vacataire->addFormation($formations);
+        $etat1 = new Etat();
+        $etat1->setLibelle("Incomplet");
+        $etat2 = new Etat();
+        $etat2->setLibelle("Inexistant");
+        $etat3 = new Etat();
+        $etat3->setLibelle("Complet");
+        
+        $dossier1 = new Dossier();
+        $dossier1->setEtat($etat1);
+        $dossier1->setVacataire($vacataire1);
+        
+        $dossier2 = new Dossier();
+        $dossier2->setEtat($etat2);
+        $dossier2->setVacataire($vacataire2);
+        
 
+        
         $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($vacataire);
+        $entityManager->persist($vacataire1);
+        $entityManager->persist($vacataire2);
+        $entityManager->persist($etat1);
+        $entityManager->persist($etat2);
+        $entityManager->persist($etat3);
+        $entityManager->persist($dossier1);
+        $entityManager->persist($dossier2);
         $entityManager->flush();
+        return $this->redirectToRoute('homepage');
     }
 
     /* END TESTS */
