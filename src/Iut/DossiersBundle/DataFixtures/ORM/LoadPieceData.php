2da246b074ebe3153326cb9257a4d6400d3531eb
TZK <thib-17@orange.fr>
2016-01-30 13:52:35 +0100

    Adding Piece fixture

---
+++ src/Iut/DossiersBundle/DataFixtures/ORM/LoadPieceData.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Iut\DossiersBundle\Controller;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Iut\DossiersBundle\Entity\Piece;
+
+class LoadPieceData implements FixtureInterface {
+
+    private $manager;
+
+    public function load(ObjectManager $manager) {
+        $this->manager = $manager;
+
+        $pieces = [
+            "Certificat de nationalité française",
+            "Pièce d’identité",
+            "Photos d’identité",
+            "Acte de naissance",
+            "Justificatifs de domicile",
+        ];
+
+        foreach ($pieces as $p)
+           $this->newPieceEntity($p);
+        $this->manager->flush();
+    }
+
+    private function newPieceEntity($data) {
+        $piece = new Piece();
+        $piece->setLibelle($data);
+        $this->manager->persist($piece);
+    }
+
+}
