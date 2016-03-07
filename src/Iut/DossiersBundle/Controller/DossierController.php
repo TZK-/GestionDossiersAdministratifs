23d66b0e024a61c5a2f0932f1ff20f3c63755e8f
TZK- <thib-17@orange.fr>
2016-03-07 10:04:54 +0100

    Updating historic view (+pagination)

--- src/Iut/DossiersBundle/Controller/DossierController.php
+++ src/Iut/DossiersBundle/Controller/DossierController.php
@@ -68,8 +68,13 @@ class DossierController extends Controller {
 
     public function historiqueDossierAction() {
         $entityManager = $this->getDoctrine()->getManager();
+        $query = $entityManager->getRepository(Dossier::class)->findClosedDossier();
+
+        $paginator = $this->get('paginator');
+        $pagination = $paginator->paginate($query, $request->query->getInt('page', 1), 10);
+
         return $this->render("IutDossiersBundle:Dossier:dossier_historique.html.twig", [
-            'dossiers' => $entityManager->getRepository(Dossier::class)->findClosedDossier()
+            'pagination' => $pagination
         ]);
     }
 }
